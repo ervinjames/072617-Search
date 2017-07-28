4d3894f0e0f3dbc70ce0d5ad43d0d000da78e1cd
ervinjames <ervinjamesvillegas@gmail.com>
2017-07-28 16:50:54 +0800

    my first commit

--- server.js
+++ server.js
@@ -10,29 +10,36 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, 'public')));
 //express routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/introduction.html'));
+  res.sendFile(path.join(__dirname, 'public/introduction.html'));
 });
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/expectations.html'));
-});app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/guidelines.html'));
-});app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/http.html'));
-});app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/hyperback.html'));
-});app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/responsive.html'));
-});app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/strategies.html'));
-});app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/TECHNIQUES.html'));
-});app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/url.html'));
+app.get('/expectations.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/expectations.html'));
 
-app.get('/about', function(req, res){
+});app.get('/guidelines.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/guidelines.html'));
+
+});app.get('/http.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/http.html'));
+
+});app.get('/hyperback.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/hyperback.html'));
+
+});app.get('/responsive.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/responsive.html'));
+
+});app.get('/strategies.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/strategies.html'));
+
+});app.get('/TECHNIQUES.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/TECHNIQUES.html'));
+
+});app.get('/url.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/url.html'));
+
+app.get('/about.html', function(req, res){
   res.sendFile(path.join(__dirname, 'views/about.html'));
 });
 app.get('*', function(req, res){
